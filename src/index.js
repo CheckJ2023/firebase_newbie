bb330d1078b4333f003b5d20125d1f7d1dc37c7c
biggerslipper <john@centos7john.appdeveloper.local>
2023-07-26 11:11:27 -0400

    modified index.js and ran command prompt:npm install firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('hello from index.js')
+
+
+//console.log('hello from index.js')
+
+//For firebase 8: import core part of firebase library 
+import firebase from 'firebase/app';
+
+//For firebase 9: import core part of firebase library 
+// import { initialize App}
+
+//config object firebaseConfig
+const firebaseConfig = {
+  apiKey: "AIzaSyA_IFmRBnnUgElLMZAWXxcplNkoeTf3azo",
+  authDomain: "fir-9-dojo-48e97.firebaseapp.com",
+  projectId: "fir-9-dojo-48e97",
+  storageBucket: "fir-9-dojo-48e97.appspot.com",
+  messagingSenderId: "47072493462",
+  appId: "1:47072493462:web:5edb41fc7af225fe7be7b7"
+};
+
+//For firebase 8:
+firebase.initializeApp(firebaseConfig)
+
+//For firebase 9:
+//initializeApp(firebaseConfig)
