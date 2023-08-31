5001460f5e0cc381e7817f8cd43f3e3efaa00cc7
bigslipper <biggerslipper@gmail.com>
2023-08-31 21:31:22 +0800

    modified index.js for connection to my database from firebase

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@
 //import firebase from 'firebase/app';
 
 //For firebase 9: import core part of firebase library 
- import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
+import {
+    getFirestore,
+    collection,
+    getDocs
+} from 'firebase/firestore';
 
 //config object firebaseConfig
 const firebaseConfig = {
@@ -18,8 +23,47 @@ const firebaseConfig = {
   appId: "1:47072493462:web:5edb41fc7af225fe7be7b7"
 };
 
+
+//vvvvvvvvvvvvvvvvvvvvvvv
+//!  init firebase app  !
+//^^^^^^^^^^^^^^^^^^^^^^^
+
 //For firebase 8:
 //firebase.initializeApp(firebaseConfig)
 
 //For firebase 9:
-initializeApp(firebaseConfig)
+initializeApp(firebaseConfig);
+
+//vvvvvvvvvvvvvvvvvvvv
+//!  init services   !
+//^^^^^^^^^^^^^^^^^^^^
+
+const db = getFirestore();
+
+//For firebase <9
+//const db = firebase.firestore()
+//db.collection('books')
+
+//vvvvvvvvvvvvvvvvvvvvv
+//!  collection ref   !
+//^^^^^^^^^^^^^^^^^^^^^
+
+const colRef = collection(db, 'books');
+
+//vvvvvvvvvvvvvvvvvvvvvvv
+//! get collection data !
+//^^^^^^^^^^^^^^^^^^^^^^^
+
+getDocs(colRef)
+  .then((snapshot) => {
+    //console.log(snapshot.docs);
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(),id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+
